2998d61256c1a426a98beffcac2ab5c760ac2251
samuelndm <samuelnmoura@gmail.com>
2021-01-26 15:59:09 -0300

    refactor: add HomePage

---
+++ src/pages/HomePage/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import * as GS from "../../assets/styles/GlobalStyles";
+
+const HomePage = () => {
+  return <GS.PageContainer></GS.PageContainer>;
+};
+
+export default HomePage;
