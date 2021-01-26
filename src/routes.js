835ff6a77ad2baf7a083cf0d0afd0a3a2582fc79
samuelndm <samuelnmoura@gmail.com>
2021-01-26 16:00:14 -0300

    refactor: add home route

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import * as P from "./pages";
 
 const Routes = () => {
   return (
     <Router>
       <Switch>
-        <Route />
+        <Route exact path='/' component={P.HomePage} />
       </Switch>
     </Router>
   );
