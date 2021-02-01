bb1e195ab0dea0a93056f38736e7bd473f2f6ff9
samuelndm <samuelnmoura@gmail.com>
2021-02-01 01:23:43 -0300

    fix: small fix at logo url

--- src/components/Navbar/Logo/index.js
+++ src/components/Navbar/Logo/index.js
@@ -1,12 +1,22 @@
 import React from "react";
+import { useCurrentPokemonTypeContext } from "../../../contexts";
 import * as S from "./styles";
 import * as UI from "../../UIComponents";
 import logo from "../../../assets/images/logo.png";
 
 const Logo = () => {
+  const { currentPokemonType } = useCurrentPokemonTypeContext();
+
   return (
     <S.Container>
-      <UI.LinkHandler url='/'>
+      <UI.LinkHandler
+        url={
+          currentPokemonType?.name?.en_us
+            ? `/tipo/${currentPokemonType?.name?.en_us}`
+            : "/"
+        }
+        key={currentPokemonType?.name?.en_us}
+      >
         <S.Image src={logo} alt='logo do site' />
       </UI.LinkHandler>
     </S.Container>
